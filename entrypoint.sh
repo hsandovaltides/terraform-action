710a5f71be2d14919aeeb20c9ccd4b1ce160baeb
Hermes Sandoval M <hsandoval@tides.cl>
2021-04-21 22:42:27 -0400

    Add variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,11 +9,10 @@ echo "::set-output name=prueba::$time"
 
 export AWS_DEFAULT_REGION=$2
 export AWS_ACCESS_KEY_ID=$3
-export AWS_ACCESS_KEY_ID=$4
-export AWS_SECRET_ACCESS_KEY=$5
-export TF_VAR_AWS_CERTIFICATE=$6
-export TF_VAR_AWS_ZONE=$7
-export TF_VAR_AWS_REGION=$8
+export AWS_SECRET_ACCESS_KEY=$4
+export TF_VAR_AWS_CERTIFICATE=$5
+export TF_VAR_AWS_ZONE=$6
+export TF_VAR_AWS_REGION=$2
 
 terraform init
 terraform workspace select $TERRAFORM_WORKSPACE || terraform workspace new $TERRAFORM_WORKSPACE
